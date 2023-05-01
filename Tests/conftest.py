983749b918fda7e57140792207bd763db6be8b57
Ashraf, Mostafa <Mostafa.Ashraf@dell.com>
2023-05-01 13:05:42 +0300

    ready to add pages and properties

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -1,11 +1,46 @@
 import pytest
-import logging
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from Utils.assertclass import AssertionBy
+
 
 @pytest.fixture(scope="session", autouse=True)
-def url(pytestconfig):
+def url():
     url = "<insert_url>"
-    logging.info('Portal Url: {}'.format(url))
     return url
+
+@pytest.fixture(scope="session", autouse=True)
+def host(pytestconfig):
+    return pytestconfig.getoption("host")
+
+
+@pytest.fixture(scope="session")
+def firefox_driver(host):
+    remote_host = "http://selenium-chrome:4444"
+    firfox_options = Options()
+    firfox_options.add_argument("--window-size=1920,1080")
+    firfox_options.add_argument("--start-fullscreen")
+    firfox_options.add_argument("--no-sandbox")
+    firfox_options.add_argument("--disable-dev-shm-usage")
+    firfox_options.set_capability("acceptInsecureCerts", True)
+    caps = DesiredCapabilities.FIREFOX.copy()
+    caps['loggingPrefs'] = {
+        'browser': 'ALL',
+        'performance': 'ALL',
+    }
+    firfox_options.add_experimental_option('perfLoggingPrefs', {
+        'enableNetwork': True,
+        'enablePage': False,
+    })
+
+    driver = webdriver.Remote(
+        command_executor=remote_host, options=firfox_options, desired_capabilities=caps)
+    yield driver
+    driver.quit()
+
+@pytest.fixture(scope="session")
+def assertion():
+    assert_fixture = AssertionBy()
+    yield assert_fixture
+    del assert_fixture
---
+++ Tests/css_properties/test_css_properties.py
@@ -0,0 +1,5 @@
+import pytest
+
+class TestCSSProperties:
+    def test_css_properties(self, assertion):
+        assertion.check_equality(expcted_css_prorty, actual_css_property)
